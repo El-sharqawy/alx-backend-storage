489d181db68a18119d9156ac4693c31f77e61b98
El-sharqawy <elsharqawy2@gmail.com>
2024-08-14 02:50:13 +0300

    MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Log Stats"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ Log Stats """
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginxLogs = client.logs.nginx
+
+    numLogs = nginxLogs.count_documents({})
+    print(f"{numLogs} logs")
+    curMethods: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in curMethods:
+        curMethod: int = nginxLogs.count_documents({"method": method})
+        print(f"\tmethod {method}: {curMethod}")
+
+    docsCount: int = nginxLogs.count_documents({"method": "GET", "path": "/status"})
+    print(f"{docsCount} status check")
